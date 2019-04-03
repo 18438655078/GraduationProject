67cf4b133dab17e747fcecac9ea996ffea5fea3c
I'teldyou <18438655078@163.com>
2019-04-03 21:19:58 +0800

    edit login and reg

--- analysis/models.py
+++ analysis/models.py
@@ -1,9 +1,22 @@
 from django.db import models
-from django import forms
+from django.contrib.auth.models import AbstractUser
+# from django import forms
 
 # Create your models here.
 
 
-class UserInfo(models.Model):
-    username = models.Model
+class UserInfo(AbstractUser):
+    username = models.CharFiled(max_length=60)
+    uid = models.AutoField(primary_key=True)
+    password = models.CharFiled(max_length=60)
+    # telephone = models.CharField(max_length=11, null=True, unique=True)
+    # avatar = models.FileField(upload_to='avatars/', default="/avatars/default.png")
+    # create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    # blog = models.OneToOneField(to='Blog', to_field='nid', null=True)
 
+
+class OrderInfo(AbstractUser):
+    username = models.CharFiled(max_length=60)
+    oid = models.AutoField(primary_key=True)
+    outkey = models.ForeignKey(UserInfo,on_delete=models.CASCADE,related_name='outkey')
+    # password = models.CharFiled(max_length=60)
--- analysis/views.py
+++ analysis/views.py
@@ -1,20 +1,82 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 import pandas as pd
+from .models import UserInfo
 
 # Create your views here.
 
-from django.utils.translation import gettext as _
-from django.core.exceptions import ValidationError
+from django.shortcuts import render, redirect
+from .models import UserProfile
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserRegisterForm, UserLoginForm
 
 
+def user_register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    else:
+        # 实例form类，用来验证用户提交的数据
+        user_register_form = UserRegisterForm(request.POST)
+        if user_register_form.is_valid():  # 返回值为True或False
+            username = user_register_form.cleaned_data['username']
+            password = user_register_form.cleaned_data['password']
 
-def login(request):
-    return render(request, 'gapp/login.html')
+            user = UserProfile.objects.filter(username=username)
+            if user:
+                return render(request, 'register.html', {
+                    'mag': '账号已存在'
+                })
+            else:
+                a = UserProfile()
+                a.password = password
+                a.username = username
+                # 内部函数自动加密功能
+                a.set_password(password)
+                a.save()
+                return redirect(reverse('login'))
+        else:
+            return render(request, 'register.html', {
+                'user_register_form': user_register_form
+            })
+
+
+def user_login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        user_login_form = UserLoginForm(request.POST)
+        if user_login_form.is_valid():
+            username = user_login_form.cleaned_data['username']
+            password = user_login_form.cleaned_data['password']
+
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect(reverse('index'))
+
+            else:
+                return render(request, 'login.html', {
+                    'msg': '用户名或者密码错误！',
+                    'user_login_form': user_login_form
+                })
+        else:
+            pass
+
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'index.html')
+
+
+#
+# def login(request):
+#     return render(request, 'gapp/login.html')
 
 
 def do_login(request):
-    pass
+    username = request.POST['username']
+    password = request.POST['password']
 
 
 def index(request):
